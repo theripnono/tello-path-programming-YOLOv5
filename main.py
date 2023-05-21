e5d13e7228375bc0d45133c65935e66453d231cf
Theripnono <theripnono@gmail.com>
2023-05-21 18:18:48 +0200

    Tello Path Planning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import math
 from pygame.locals import *
 from djitellopy import Tello
 
@@ -9,33 +10,74 @@ pygame.init()
 # Initialize Tello drone
 tello = Tello()
 
-
 # Set up a delay to ensure a stable connection
-time.sleep(2)
+time.sleep(1)
+
 # Set up the window
 window_width, window_height = 500, 500
 window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Draw Path")
+pygame.display.set_caption("Draw Drone Path")
 
 # Set up colors
-RED = (255, 0, 0)
+RED = (194, 24, 7)
 BLUE = (0, 0, 255)
+GREEN = (46, 139, 87)
 
 # Set up the starting position
 start_pos = [window_width // 2, window_height // 2]
+start_pos2 = [window_width // 2, window_height // 2]  # new start position
 
 # Set up variables for tracking path
 path = []
+draw_path = []
+letter_path = []
 drawing = False
+
+
+# Set up the triangle coordinates
+triangle_side_length = 10
+half_triangle_side = triangle_side_length // 2
+triangle_height = int(triangle_side_length * math.sqrt(3) / 2)
+triangle_points = [
+    (start_pos[0] - half_triangle_side, start_pos[1] + triangle_height),
+    (start_pos[0], start_pos[1] - triangle_height),
+    (start_pos[0] + half_triangle_side, start_pos[1] + triangle_height)
+]
+
+# Set up the triangle rotation angle
+triangle_rotation = 0
+
 
 # Set up variables for animation
 animate_dot = False
 dot_pos = list(start_pos)
+start_pos2 = [window_width // 2, window_height // 2]
+
 dot_speed = 2
 dot_index = 0
+green_dots = []
 last_move_time = time.time()
+last_move = []
 
-# Main game loop
+
+"""
+New triangle used to track
+"""
+# Set up the triangle coordinates
+triangle_side_length2 = 10
+half_triangle_side2 = triangle_side_length // 2
+triangle_height2 = int(triangle_side_length * math.sqrt(3) / 2)
+triangle_points2 = [
+    (start_pos2[0] - half_triangle_side2, start_pos2[1] + triangle_height2),
+    (start_pos2[0], start_pos2[1] - triangle_height2),
+    (start_pos2[0] + half_triangle_side2, start_pos2[1] + triangle_height2)
+]
+
+# Set up the triangle rotation angle
+triangle_rotation2 = 0
+
+for_loop_finished = False
+# Main loop
 running = True
 while running:
     # Handle events
@@ -48,68 +90,144 @@ while running:
                     animate_dot = True
                     dot_index = 0
                     last_move_time = time.time()
-
-                    tello.connect()
-                    # Set up a delay to ensure a stable connection
-                    time.sleep(2)
-                    tello.takeoff()
-            elif event.key == K_a:
-                start_pos[0] -= 10
-                path.append(tuple(start_pos))
-            elif event.key == K_d:
-                start_pos[0] += 10
-                path.append(tuple(start_pos))
-            elif event.key == K_w:
+                    #tello.connect()
+                    # tello.takeoff() # Set up a delay to ensure a stable connection
+            elif event.key == K_UP:
                 start_pos[1] -= 10
                 path.append(tuple(start_pos))
-            elif event.key == K_s:
+                letter_path.append('up')
+            elif event.key == K_DOWN:
                 start_pos[1] += 10
                 path.append(tuple(start_pos))
-            elif event.key == K_q:
-                tello.land()
+                letter_path.append('down')
+            elif event.key == K_LEFT:
+                start_pos[0] -= 10
+                path.append(tuple(start_pos))
+                letter_path.append('left')
+            elif event.key == K_RIGHT:
+                start_pos[0] += 10
+                path.append(tuple(start_pos))
+                letter_path.append('right')
+            elif event.key == K_a:
+                triangle_rotation -= 90
+                green_dots.append(tuple(start_pos))
+                letter_path.append('a')
+            elif event.key == K_d:
+                triangle_rotation += 90
+                green_dots.append(tuple(start_pos))
+                letter_path.append('d')
+            #Other options
+            elif event.key == K_r:
+                path = []  # Reset the path
+                green_dots = []  # Reset dots
+                letter_path = []  # Reset letters
+                draw_path = []  # Reset blue path
+                start_pos = [window_width // 2, window_height // 2]
+                start_pos2 = start_pos
+            elif event.key == K_q:  # Abort mission
+                #tello.land()
+                print("abort")
 
-    # Draw the window and circle
-    window.fill((255, 255, 255))
-    pygame.draw.circle(window, RED, start_pos, 5)
+    # Update the triangle coordinates based on the new start_pos
+    triangle_points = [
+        (start_pos[0], start_pos[1] - half_triangle_side),
+        (start_pos[0] - half_triangle_side, start_pos[1] + half_triangle_side),
+        (start_pos[0] + half_triangle_side, start_pos[1] + half_triangle_side)
+    ]
+    # Rotate the triangle
+    rotated_points = []
+    for point in triangle_points:
+        rotated_x = (point[0] - start_pos[0]) * math.cos(math.radians(triangle_rotation)) - (
+                    point[1] - start_pos[1]) * math.sin(math.radians(triangle_rotation)) + start_pos[0]
+        rotated_y = (point[0] - start_pos[0]) * math.sin(math.radians(triangle_rotation)) + (
+                    point[1] - start_pos[1]) * math.cos(math.radians(triangle_rotation)) + start_pos[1]
+        rotated_points.append((rotated_x, rotated_y))
+    triangle_points = rotated_points
+
+    # Draw the window
+    window.fill((220, 220, 220))
+    pygame.draw.polygon(window, RED, triangle_points, 0)
+
+    for dot in green_dots:
+        pygame.draw.circle(window, GREEN, dot, 5)
 
     # Draw the path
     if len(path) >= 2:
-        pygame.draw.lines(window, RED, False, path, 2)
+        pygame.draw.lines(window, RED, False, path, 5)
 
-    # Animate the dot along the path
+    # Animate the dron path along the red path
     if animate_dot:
-        if dot_index < len(path):
-            if time.time() - last_move_time >= .1:
-                target_pos = path[dot_index]
-                direction = [target_pos[0] - dot_pos[0], target_pos[1] - dot_pos[1]]
-                length = pygame.math.Vector2(direction).length()
-                normalized_direction = [direction[0] / length, direction[1] / length]
-                dot_pos[0] += normalized_direction[0] * dot_speed
-                dot_pos[1] += normalized_direction[1] * dot_speed
 
-                if pygame.math.Vector2(target_pos).distance_to(pygame.math.Vector2(dot_pos)) < dot_speed:
-                    dot_pos = list(target_pos)
-                    dot_index += 1
-                    print(dot_index,dot_pos)
-                    # Move the Tello drone based on the dot's position
-                    tello.move_forward(20)
-                    time.sleep(0.5)
-                    # Adjust the distance as needed
-                    print("TELLO SE MUEVE")
-                last_move_time = time.time()
 
-        pygame.draw.circle(window, BLUE, dot_pos, 3)
+        """
+        To track the path I pass the given instructions that I have append before
+        Start moving dron automatically
+        """
+        if dot_index < len(letter_path):
+            l = letter_path[dot_index]
+            if l == 'up':
+                #tello.move_forward()
+                time.sleep(.5)
+                start_pos2[1] -= 10
+                draw_path.append(tuple(start_pos2))
+                print("UP")
+            elif l == 'down':
+                #tello.move_back()
+                start_pos2[1] += 10
+                draw_path.append(tuple(start_pos2))
+                time.sleep(.5)
+                print("donw")
+            elif l == 'left':
+                # tello.move_left()
+                start_pos2[0] -= 10
+                draw_path.append(tuple(start_pos2))
+                time.sleep(.5)
+                print("left")
+            elif l == 'right':
+                # tello.move_right()
+                start_pos2[0] += 10
+                draw_path.append(tuple(start_pos2))
+                time.sleep(.5)
+                print("right")
 
-    # Update the displayQ
+            elif l == 'a':
+                triangle_rotation2 -= 90
+                #tello.rotate_clockwise(-90)
+
+            elif l == 'd':
+                triangle_rotation2 += 90
+                #tello.rotate_clockwise(90)
+
+            dot_index +=1
+
+            # Update the triangle coordinates based on the new start_pos
+            triangle_points2 = [
+                (start_pos2[0], start_pos2[1] - half_triangle_side2),
+                (start_pos2[0] - half_triangle_side2, start_pos2[1] + half_triangle_side2),
+                (start_pos2[0] + half_triangle_side2, start_pos2[1] + half_triangle_side2)
+            ]
+            # Rotate the triangle
+            rotated_points2 = []
+
+            for point in triangle_points2:
+                rotated_x = (point[0] - start_pos2[0]) * math.cos(math.radians(triangle_rotation2)) - (
+                        point[1] - start_pos2[1]) * math.sin(math.radians(triangle_rotation2)) + start_pos2[0]
+
+                rotated_y = (point[0] - start_pos2[0]) * math.sin(math.radians(triangle_rotation2)) + (
+                        point[1] - start_pos2[1]) * math.cos(math.radians(triangle_rotation2)) + start_pos2[1]
+                rotated_points2.append((rotated_x, rotated_y))
+            triangle_points2 = rotated_points2
+            pygame.draw.polygon(window, BLUE, triangle_points2, 0)
+
+    # Draw the path
+    if len(draw_path) >= 2:
+        pygame.draw.lines(window, BLUE, False, draw_path, 5)
+
+    # Update the display
     pygame.display.update()
-
-# Print the path coordinates
-print("Path coordinates:")
-for coord in path:
-    print(coord)
 
 # Quit Pygame
 pygame.quit()
 
 # Disconnect from the Tello drone
-tello.disconnect()
+#tello.disconnect()
